5ecc300d76b0afc65417b0223a6a05c5c712c969
Tushar Randhe <121237922+tusharrandhe@users.noreply.github.com>
2023-02-24 16:32:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const li = document.getElementsByName("li");
-console.log(li.length);
-let yourArray = ["one string",241,true];
+// console.log(li.length);
+let yourArray = ["one string",241,true,"Two String",3554,false];
